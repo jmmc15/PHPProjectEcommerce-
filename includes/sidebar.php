59a64ae47fb64c03783e430280bc0e71dd426f7a
Jules Miko Cuevas <julesmiko.cuevas.cics@ust.edu.ph>
2022-12-06 14:11:50 +0800

    update

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,3 +1,7 @@
+<?php
+ include_once('functions/functions.php');
+?>
+
 <div class="panel panel-default sidebar-menu"> <!-- panel panel-default sidebar-menu Begin -->
   <div class="panel-heading"> <!-- panel-heading Begin -->
     <h3 class="panel-title">Products Categories</h3>
